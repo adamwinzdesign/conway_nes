cdb1c4c852302a8bdbed7d61a53fdafd3c3b4b9b
adamwinzdesign <adamwinzdesign@gmail.com>
2020-05-29 10:55:24 -0500

    rearranged element position

--- src/App.js
+++ src/App.js
@@ -130,14 +130,14 @@ const generateEmptyGrid = (col, row) => {
 }
 
 function App() {
-  const [numCol, setCols] = useState(30)
-  const [numRow, setRows] = useState(30)
+  const numCol = 30
+  const numRow = 30
   const [grid, setGrid] = useState(() => {
     return generateEmptyGrid(numCol, numRow)
   })
   const [fast, setFast] = useState(false)
-  const [med, setMed] = useState(true)
-  const [slow, setSlow] = useState(false)
+  const [med, setMed] = useState(false)
+  const [slow, setSlow] = useState(true)
   const [speed, setSpeed] = useState(100)
   const [red, setRed] = useState(false)
   const [black, setBlack] = useState(true)
@@ -145,6 +145,7 @@ function App() {
   const [yellow, setYellow] = useState(false)
   const [color, setColor] = useState('black')
   const [running, setRunning] = useState(false)
+  const [generation, setGeneration] = useState(0)
 
   useEffect(() => {
     if(red === true){
@@ -168,6 +169,10 @@ function App() {
     }
   }, [fast, med, slow])
 
+  const incrementGeneration = (generation) => {
+    setGeneration(generation + 1)
+  }
+
   const runningRef = useRef(running)
   runningRef.current = running
 
@@ -177,18 +182,21 @@ function App() {
     setBlack(false)
     setRed(true)
   }
+
   const changeBlack = () => {
     setYellow(false)
     setOrange(false)
     setRed(false)
     setBlack(true)
   }
+
   const changeOrange = () => {
     setYellow(false)
     setRed(false)
     setBlack(false)
     setOrange(true)
   }
+
   const changeYellow = () => {
     setOrange(false)
     setRed(false)
@@ -201,11 +209,13 @@ function App() {
     setSlow(false)
     setFast(true)
   }
+
   const changeMed = () => {
     setSlow(false)
     setFast(false)
     setMed(true)
   }
+
   const changeSlow = () => {
     setFast(false)
     setMed(false)
@@ -213,11 +223,14 @@ function App() {
   }
 
   const runSimulation = useCallback(() => {
+    // incrementGeneration(generation)
     if (!runningRef.current){
       return
     }
     setGrid(g => {
+      // incrementGeneration(generation)
       return produce(g, gridCopy => {
+        // incrementGeneration(generation)
         for(let i = 0; i < numRow; i++){
           for(let k = 0; k < numCol; k++){
             let neighbors = 0
@@ -238,55 +251,19 @@ function App() {
       })
     })
     
+    // incrementGeneration(generation)
+    setTimeout(incrementGeneration(generation), speed)
     setTimeout(runSimulation, speed)
-  }, [speed, numCol, numRow])
+  }, [speed, numCol, numRow, generation])
 
-  console.log(grid)
+  // console.log(grid)
 
   return (
     <>
+      {/* entire page */}
       <div className='main'>
-        <div className='buttons nes-container with-title'>
-          <p className='title'>Controls</p>
-          <button
-            className='nes-btn'
-            onClick={() => {
-              setRunning(!running)
-              if(!running){
-                runningRef.current = true
-                runSimulation()
-              }
-            }}
-          >
-            {running ? 'Stop' : 'Start'}
-          </button>
-          <button 
-            className='nes-btn'
-            onClick={() => {
-              if(running){
-                setRunning(!running)
-              }
-              setGrid(generateEmptyGrid(numCol, numRow))
-            }}
-          >
-            Clear
-          </button>
-          <button 
-            className='nes-btn'
-            onClick={() => {
-              if(running){
-                setRunning(!running)
-              }
-              const rows = []
-              for(let i=0; i < numRow; i++){
-                rows.push(Array.from(Array(numCol), () => Math.random() > .7 ? 1 : 0))
-              }  
-              setGrid(rows)
-            }}
-          >
-            Randomize
-          </button>
-        </div>
+
+        {/* title and grid */}
         <div className='header'>
           <h1>Conway's Game of Life!</h1>
 
@@ -316,72 +293,115 @@ function App() {
               ))
             )}
           </div>
-          <div className='bottom'>
-            <div className='presets nes-container with-title is-centered'>
-              <p className='title'>Presets</p>
-              <button 
-              className='nes-btn'
-              onClick={() => {
-                setRows(30)
-                setCols(30)
-                setGrid(presetOne())
-                }}
-              >
-                Preset One
-              </button>
-              <button 
-              className='nes-btn'
-              onClick={() => {
-                setRows(30)
-                setCols(30)
-                setGrid(presetTwo())
-                }}
-              >
-                Preset Two
-              </button>
-              <button 
-              className='nes-btn'
-              onClick={() => {
-                setRows(30)
-                setCols(30)
-                setGrid(presetThree())
-                }}
-              >
-                Preset Three
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className='colors nes-container with-title'>
-          <p className='title'>Preferences</p>
-          <div className='columns'>
-            <div className='flexes'>
-              <button className={red ? 'red colorbut': 'colorbut redout'} onClick={() => changeRed()}/>
-              <button className={black ? 'black colorbut' : 'colorbut'} onClick={() => changeBlack()}/>
-            </div>
-            <div className='flexes'>
-              <button className={orange ? 'orange colorbut': 'colorbut orangeout'} onClick={() => changeOrange()}/>
-              <button className={yellow ? 'yellow colorbut' : 'colorbut yellowout'} onClick={() => changeYellow()}/>
-            </div>
-          </div>
-          <button className={fast ? 'current speed nes-btn' : 'speed nes-btn'} onClick={() => changeFast()}>fast</button>
-          <button className={med ? 'current speed nes-btn' : 'speed nes-btn'} onClick={() => changeMed()}>medium</button>
-          <button className={slow ? 'current speed nes-btn' : 'speed nes-btn'} onClick={() => changeSlow()}>slow</button>
-        </div>
 
-        <div className='nes-container with-title'>
+          <div className='nes-container with-title'>
             <p className='title'>Rules</p>
             <div className="lists">
               <ul className="nes-list is-disc">
                 <li>Each cell will live or die at the next generation depending on how many neighbors it has currently.</li>
-                <li>Cells with 2 or fewer neighbors will die.</li>
-                <li>Cells with 3 neighbors will live into the next generation.</li>
-                <li>Cells with 4 or more neighbors will die.</li>
+                <li>Live cells with 2 or 3 neighbors will survive into the next generation</li>
+                <li>Any dead cell with 3 live neighbors will become a live cell.</li>
+                <li>Live cells with only one neighbor or with 4 or more neighbors will die.</li>
                 <li>The process will repeat until you click stop!</li>
               </ul>
             </div>
           </div>
+        </div>
 
+        <div className='extras'>
+          <div className='presets nes-container with-title is-centered'>
+            <p className='title'>Presets</p>
+            <button 
+              className='nes-btn'
+              onClick={() => {
+                setGrid(presetOne())
+              }}
+            >
+              Preset One
+            </button>
+            <button 
+              className='nes-btn'
+              onClick={() => {
+                setGrid(presetTwo())
+              }}
+            >
+              Preset Two
+            </button>
+            <button 
+              className='nes-btn'
+              onClick={() => {
+                setGrid(presetThree())
+              }}
+            >
+              Preset Three
+            </button>
+          </div>
+          
+          <div className='colors nes-container with-title'>
+            <p className='title'>Preferences</p>
+            <div className='columns'>
+              <div className='flexes'>
+                <button className={red ? 'red colorbut': 'colorbut redout'} onClick={() => changeRed()}/>
+                <button className={black ? 'black colorbut' : 'colorbut'} onClick={() => changeBlack()}/>
+              </div>
+              <div className='flexes'>
+                <button className={orange ? 'orange colorbut': 'colorbut orangeout'} onClick={() => changeOrange()}/>
+                <button className={yellow ? 'yellow colorbut' : 'colorbut yellowout'} onClick={() => changeYellow()}/>
+              </div>
+            </div>
+            <button className={fast ? 'current speed nes-btn' : 'speed nes-btn'} onClick={() => changeFast()}>Fast</button>
+            <button className={med ? 'current speed nes-btn' : 'speed nes-btn'} onClick={() => changeMed()}>Medium</button>
+            <button className={slow ? 'current speed nes-btn' : 'speed nes-btn'} onClick={() => changeSlow()}>Slow</button>
+          </div>
+              
+          <div className='buttons nes-container with-title'>
+            <p className='title'>Controls</p>
+            <button
+              className='nes-btn'
+              onClick={() => {
+                setRunning(!running)
+                if(!running){
+                  runningRef.current = true
+                  runSimulation()
+                }
+              }}
+            >
+              {running ? 'Stop' : 'Start'}
+            </button>
+            <button 
+              className='nes-btn'
+              onClick={() => {
+                if(running){
+                  setRunning(!running)
+                }
+                setGrid(generateEmptyGrid(numCol, numRow))
+              }}
+            >
+              Clear
+            </button>
+            <button 
+              className='nes-btn'
+              onClick={() => {
+                if(running){
+                  setRunning(!running)
+                }
+                const rows = []
+                for(let i=0; i < numRow; i++){
+                  rows.push(Array.from(Array(numCol), () => Math.random() > .7 ? 1 : 0))
+                }  
+                setGrid(rows)
+              }}
+            >
+              Randomize
+            </button>
+          </div>
+          
+          <div className='nes-container with-title generationcontain'>
+            <p className='title'>Generation</p>
+            <span className='generation'>{generation}</span>
+          </div>
+
+        </div>
       </div>
     </>
   )
